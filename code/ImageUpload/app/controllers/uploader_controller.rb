505fad9392b444eed43e0ff662028e047b8edfd0
lukecrum20 <lukecrum20@gmail.com>
2018-05-22 13:09:44 -0400

    file fix hopefully

--- code/ImageUpload/app/controllers/uploader_controller.rb
+++ code/ImageUpload/app/controllers/uploader_controller.rb
@@ -5,8 +5,9 @@ class UploaderController < ApplicationController
     file = params["Photo"][:file]
     n = YAML.load_file('/var/www/html/filename.yml')
     name = n['name']
+    new_name = name.to_s + File.extname(file.original_filename)
     n['name'] = n['name'] + 1
     File.open('/var/www/html/filename.yml', 'w') { |f| f.write n.to_yaml }
-    FileUtils.mv(file.original_filename, "/var/www/html/images/#{file.original_filename}")
+    File.rename file.original_filename "/var/www/html/images/#{new_name}"
   end
 end
